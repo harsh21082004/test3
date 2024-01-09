baa416772a101582702405b66b7cc3e1dc5c89db
Harsh Tiwari <131223935+harsh21082004@users.noreply.github.com>
2024-01-09 16:24:14 +0530

    first commit

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const connectDb = handler => async (req,res)=>{
     if(mongoose.connections[0].readyState){
         return handler(req,res)
     }
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect('mongodb+srv://harshtiwariup2004:harsh2004@cluster0.y3do7jo.mongodb.net/codebyte')
     return handler(req,res)
 }
 
